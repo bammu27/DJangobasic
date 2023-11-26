9d6b1ee5e18bf3c68b2cef7dc015e23d6f438e21
Brahmaraj kashetti <mahaveerkashetti203@gmail.com>
2023-11-26 22:25:11 +0530

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=20)
+    comleted = models.BooleanField(default=False)
+
+
+    
